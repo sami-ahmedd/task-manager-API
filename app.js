bd2e3694026b1efefc67f797decf8f20f0075755
Sami Ahmed <syedsami2005@gmail.com>
2025-09-28 09:42:01 +0530

    create route and controller directories

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+
+app.use(express.json())
+
+
+app.get('/hello', (req,res) => {
+    res.send("hello")
+})
+
+app.use('/api/v1/tasks',tasks)
+
+app.listen(5000, () => console.log('listening on port 5000'))
---
+++ controllers/tasks.js
@@ -0,0 +1,7 @@
+const getAllTasks = (req,res)=>{
+    res.send('get all tasks')
+}
+
+module.exports = {
+    getAllTasks
+}

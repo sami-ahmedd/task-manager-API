c8e334c9656161417ce7ee754b3d07cfd0fc93dc
Sami Ahmed <syedsami2005@gmail.com>
2025-09-29 08:13:38 +0530

    created database schema

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
 
 app.use(express.json())
 
@@ -12,4 +14,14 @@ app.get('/hello', (req,res) => {
 app.use('/api/v1/tasks',tasks)
 
 const port = 3000;
-app.listen(port, () => console.log('listening on port 3000'))
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, () => console.log('listening on port 3000'))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
--- db/connect.js
+++ db/connect.js
@@ -1,2 +1,9 @@
-const connectionString = 
-'mongodb+srv://syedsami2005_db_user:<db_password>@nodeexpressprojects.n4rcjgu.mongodb.net/?retryWrites=true&w=majority&appName=NodeExpressProjects' 
+const mongoose = require('mongoose')
+
+
+
+const connectDB = (url) => {
+    return mongoose.connect(url)
+}
+
+module.exports = connectDB
